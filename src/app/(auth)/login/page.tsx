85cff6bc1ab5d78e399e6cd09cc4fe0186122e64
quanxenon5525 <quanxenon5525@gmail.com>
2023-12-25 23:19:45 +0700

    pushhhh

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,7 +1,41 @@
-import { Chip } from "@mui/material";
-import { FormLogin } from "./FormLogin";
+"use client";
+import React from "react";
+import { Checkbox, Form, Input, Button, Flex } from "antd";
+import { Chip, Stack } from "@mui/material";
+import { useRouter } from "next/navigation";
+import Link from "next/link";
+
+type FieldType = {
+  username?: string;
+  password?: string;
+  remember?: string;
+};
 
 export const page = () => {
+  const router = useRouter();
+
+  const onFinish = (values: any) => {
+    const { username, password } = values;
+    if (username && password) {
+      router.push("/");
+    }
+  };
+
+  const onFinishFailed = (errorInfo: any) => {
+    console.log("Failed:", errorInfo);
+  };
+
+  const formItemLayout = {
+    labelCol: {
+      xs: { span: 24 },
+      sm: { span: 120 },
+    },
+    wrapperCol: {
+      xs: { span: 24 },
+      sm: { span: 120 },
+    },
+  };
+
   return (
     <div className="pt-20 mt-20">
       <div className="text-black text-center mb-10">
@@ -11,8 +45,65 @@ export const page = () => {
           color="warning"
           label="Vui lòng đăng nhập để sử dụng dịch vụ"
         />
-        <FormLogin />
       </div>
+      <Form
+        {...formItemLayout}
+        className="w-full flex flex-wrap flex-col content-center"
+        name="basic"
+        initialValues={{ remember: true }}
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+        autoComplete="off"
+        labelAlign="left"
+        labelWrap
+      >
+        <Form.Item<FieldType>
+          label="Tên đăng nhập"
+          name="username"
+          rules={[{ required: true, message: "Vui lòng nhập tên đăng nhập!" }]}
+        >
+          <Input />
+        </Form.Item>
+        <Form.Item<FieldType>
+          label="Mật khẩu"
+          name="password"
+          rules={[{ required: true, message: "Vui lòng nhập mật khẩu!" }]}
+        >
+          <Input.Password />
+        </Form.Item>
+        <Form.Item<FieldType>
+          name="remember"
+          valuePropName="checked"
+          wrapperCol={{ offset: 0, span: 20 }}
+        >
+          <Stack sx={{ display: "flex", flexDirection: "column", gap: 2 }}>
+            <Checkbox>Remember me</Checkbox>
+            <Link href="/sign-up">Bạn chưa có tài khoản? hãy đăng ký</Link>
+          </Stack>
+        </Form.Item>
+        <Form.Item className="text-center gap-y-1.5">
+          <Button
+            style={{
+              width: "300px",
+              backgroundColor: "#d9534f",
+              color: "white",
+            }}
+            htmlType="submit"
+          >
+            Login
+          </Button>
+        </Form.Item>
+
+        <Button
+          style={{
+            backgroundColor: "#1677ff",
+            color: "white",
+          }}
+          className="site-button-ghost-wrapper"
+        >
+          <Link href="/">Quay lại trang chủ</Link>
+        </Button>
+      </Form>
     </div>
   );
 };
--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -1,13 +1,165 @@
+"use client";
 import { Chip } from "@mui/material";
-import { SignUpForm } from "./SignUpForm";
+import { Button, Form, Input, Select } from "antd";
+import { useRouter } from "next/navigation";
+
+const { Option } = Select;
+
+/* eslint-disable no-template-curly-in-string */
+const validateMessages = {
+  required: "${label} is required!",
+  types: {
+    email: "${label} is not a valid email!",
+    number: "${label} is not a valid number!",
+  },
+  number: {
+    range: "${label} must be between ${min} and ${max}",
+  },
+};
+
+const prefixSelector = (
+  <Form.Item name="prefix" noStyle>
+    <Select defaultValue="84" style={{ width: 70 }}>
+      <Option value="84">+84</Option>
+    </Select>
+  </Form.Item>
+);
+
+const formItemLayout = {
+  labelCol: {
+    xs: { span: 24 },
+    sm: { span: 8 },
+  },
+  wrapperCol: {
+    xs: { span: 24 },
+    sm: { span: 16 },
+  },
+};
+
+const tailFormItemLayout = {
+  wrapperCol: {
+    xs: {
+      span: 24,
+      offset: 0,
+    },
+    sm: {
+      span: 16,
+      offset: 8,
+    },
+  },
+};
+
+type FieldType = {
+  username?: string;
+  password?: string;
+  email?: string;
+  confirmPassword?: string;
+  phone?: string;
+};
 
 export const page = () => {
+  const router = useRouter();
+  const onFinish = (values: any) => {
+    if (values) {
+      router.push("/login");
+    }
+    console.log(values);
+  };
+
+  const onFinishFailed = (errorInfo: any) => {
+    console.log("Failed:", errorInfo);
+  };
   return (
     <div className="p-20">
       <div className="text-black text-center mb-10">
         <Chip variant="outlined" size="large" color="primary" label="ĐĂNG KÝ" />
       </div>
-      <SignUpForm />
+      <Form
+        {...formItemLayout}
+        name="register"
+        onFinish={onFinish}
+        style={{
+          marginLeft: "25%",
+          maxWidth: "50%",
+        }}
+        scrollToFirstError
+        // layout="vertical"
+        labelWrap
+        labelAlign="left"
+        autoComplete="off"
+        // validateMessages={validateMessages}
+      >
+        <Form.Item<FieldType>
+          name="username"
+          label="Tên đăng nhập"
+          rules={[{ required: true, message: "Vui lòng nhập tên đăng nhập!" }]}
+        >
+          <Input />
+        </Form.Item>
+        <Form.Item<FieldType>
+          name="phone"
+          label="Số điện thoại"
+          rules={[{ required: true, message: "Vui lòng nhập số điện thoại!" }]}
+        >
+          <Input addonBefore={prefixSelector} style={{ width: "100%" }} />
+        </Form.Item>
+        <Form.Item<FieldType>
+          name="email"
+          label="Email"
+          rules={[
+            {
+              type: "email",
+              message: "Email không đúng định dạng!",
+            },
+            {
+              required: true,
+              message: "Vui lòng nhập Email!",
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+        <Form.Item<FieldType>
+          label="Password"
+          name="password"
+          rules={[
+            {
+              required: true,
+              message: "Vui lòng nhập mật khẩu!",
+            },
+          ]}
+        >
+          <Input.Password />
+        </Form.Item>
+        <Form.Item<FieldType>
+          label="Confirm Password"
+          name="confirmPassword"
+          dependencies={["password"]}
+          rules={[
+            {
+              required: true,
+              message: "Vui lòng nhập mật khẩu xác nhận!",
+            },
+            ({ getFieldValue }) => ({
+              validator(_, value) {
+                if (!value || getFieldValue("password") === value) {
+                  return Promise.resolve();
+                }
+                return Promise.reject(
+                  new Error("Mật khẩu xác nhận không khớp!")
+                );
+              },
+            }),
+          ]}
+        >
+          <Input.Password />
+        </Form.Item>
+        <Form.Item {...tailFormItemLayout}>
+          <Button className="text-center gap-y-1.5" htmlType="submit">
+            Register
+          </Button>
+        </Form.Item>
+      </Form>
     </div>
   );
 };
